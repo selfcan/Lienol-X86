fdef110c1a800efe99af2b0004db8fda9c9d17b4
selfcan <99958756+selfcan@users.noreply.github.com>
2023-01-16 18:58:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,60 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.101/g' package/base-files/files/bin/config_generate
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/default-settings/files/zzz-default-settings
+# 重新添加 luci-app-adbyby-plus
+#rm -rf feeds/luci/applications/luci-app-adbyby-plus
+git clone https://github.com/selfcan/luci-app-adbyby-plus-lite.git package/luci-app-adbyby-plus
+add luci-app-my-dnshelper
+git clone https://github.com/selfcan/openwrt-my-dnshelper.git package/luci-app-my-dnshelper
+add luci-app-filebrowser
+git clone https://github.com/tcsr200722/luci-app-filebrowser.git package/luci-app-filebrowser
+#git clone https://github.com/openwrt-xiaomi/luci-app-filebrowser.git package/luci-app-filebrowser
+#git clone https://github.com/takayukileung/luci-app-filebrowser.git package/luci-app-filebrowser
+add luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+add luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+add luci-app-dnsfilter
+#git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+#git clone https://github.com/kongfl888/luci-app-dnsfilter.git package/luci-app-dnsfilter
+add luci-app-autotimeset
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+add luci-app-easymesh
+#git clone https://github.com/tcsr200722/luci-app-easymesh.git package/luci-app-easymesh
+add luci-app-diskman
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+add luci-app-oaf
+#git clone https://github.com/tcsr200722/OpenAppFilter.git package/luci-app-oaf
+add luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+add luci-app-gowebdav
+git clone https://github.com/immortalwrt/openwrt-gowebdav.git package/luci-app-gowebdav
+add luci-app-aliyundrive-webdav
+#git clone https://github.com/tcsr200722/aliyundrive-webdav.git package/luci-app-aliyundrive-webdav
+add luci-app-jellyfin
+git clone https://github.com/tcsr200722/luci-app-jellyfin.git package/luci-app-jellyfin
+add luci-app-podminidlna
+#git clone https://github.com/lisaac/luci-app-podminidlna.git package/luci-app-podminidlna
+add luci-app-podsamba
+#git clone https://github.com/lisaac/luci-app-podsamba.git package/luci-app-podsamba
+add luci-app-alist
+git clone https://github.com/sbwml/openwrt-alist.git package/luci-app-alist
+# 重新添加 luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-netdata
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+add go-aliyun-webdav
+git clone https://github.com/jerrykuku/go-aliyundrive-webdav.git
+git clone https://github.com/jerrykuku/luci-app-go-aliyundrive-webdav.git package/luci-app-go-aliyundrive-webdav
+add luci-app-aliyundrive-fuse
+git clone https://github.com/messense/aliyundrive-fuse.git package/luci-app-aliyundrive-fuse
+add luci-app-bypass
+#git clone https://github.com/kiddin9/openwrt-bypass.git package/luci-app-bypass
+./scripts/feeds update -a
+./scripts/feeds install -a
